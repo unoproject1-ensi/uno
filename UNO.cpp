3ca208cd5269134b8f867a92ff4736dcadb47e03
takwacherif <63345801+takwacherif@users.noreply.github.com>
2020-05-03 13:49:07 +0200

    Update UNO.cpp

--- UNO.cpp
+++ UNO.cpp
@@ -14,11 +14,11 @@ bool UNO::fin_de_cartes()
     else
         return(false);
 }
-carte UNO::derniere_carte()
+carte UNO::derniere_carte()//retourne la derniere carte jetée
 {
     return(vjeter[vjeter.size()-1]);
 }
-void UNO::nouvelle_partie()
+void UNO::nouvelle_partie()//commencer une partie
 {
     if(vuno.size()!=0)
         vuno.clear();
@@ -29,18 +29,18 @@ void UNO::nouvelle_partie()
     for(int i=0;i<4;i++)
     {
         vuno.push_back(carte(tcoul[i],0,"0"));
-        for(int j=1;j<10;j++)
+        for(int j=1;j<10;j++)//création des cartes numérotées
         {
             vuno.push_back(carte(tcoul[i],j,to_string(j)));
             vuno.push_back(carte(tcoul[i],j,to_string(j)));
         }
-        for(int k=0;k<3;k++)
+        for(int k=0;k<3;k++)//création des cartes +2 , block et inverse
         {
             vuno.push_back(carte(tcoul[i],20,tsym[k]));
             vuno.push_back(carte(tcoul[i],20,tsym[k]));
         }
     }
-    for(int i=0;i<4;i++)
+    for(int i=0;i<4;i++)//création des cartes spéciales +4 et joker
     {
         vuno.push_back(carte("noir",50,"+4"));
         vuno.push_back(carte("noir",50,"joker"));
@@ -50,16 +50,16 @@ void UNO::nouvelle_tour()
 {
     carte c;
     c=vjeter[vjeter.size()-1];
-    vjeter.erase(vjeter.begin()+vjeter.size()-1);
-    vuno.swap(vjeter);
-    for(int i=0;i<vuno.size();i++)
+    vjeter.erase(vjeter.begin()+vjeter.size()-1);//supprimer la dernière carte avant de transférer les cartes dans vuno
+    vuno.swap(vjeter);//transférer les cartes de vjeter dans vuno
+    for(int i=0;i<vuno.size();i++)//ramener les cartes à l'etat initial(avant la modification du couleur)
     {
         if((vuno[i].donner_symbole()=="joker")||(vuno[i].donner_symbole()=="+4"))
             vuno[i].modifier_couleur("noir");
     }
-    vjeter.push_back(c);
+    vjeter.push_back(c);//remettre la dernière carte dans vjeter
 }
-ostream& operator<<(ostream& s,UNO& u)
+ostream& operator<<(ostream& s,UNO& u)//surcharge de l'opérateur << va être utiliser dans l'enregistrement
 {
     s<<u.vuno.size()<<" ";
     for(int i=0;i<u.vuno.size();i++)
