873a88c00a2d7f4653bffa03f021a471cf89b128
takwacherif <63345801+takwacherif@users.noreply.github.com>
2020-05-03 23:38:46 +0200

    Update carte.cpp

--- carte.cpp
+++ carte.cpp
@@ -9,19 +9,19 @@ carte::carte(string coul,int val,string sym)
     valeur=val;
     symbole=sym;
 }
-int carte::donner_valeur()
+int carte::donner_valeur()//retourne la valeur du carte
 {
     return(valeur);
 }
-string carte::donner_couleur()
+string carte::donner_couleur()//retourne la couleur du carte
 {
     return(couleur);
 }
-string carte::donner_symbole()
+string carte::donner_symbole()//retourne le symbole du carte
 {
     return(symbole);
 }
-void carte::modifier_couleur(string coul)
+void carte::modifier_couleur(string coul)//modifier la couleur du carte
 {
     couleur=coul;
 }
